97a586de508ae7cde8a790d971d8a095d37d608e
Arthur De Souza Silva <arthurdesouza.silv@gmail.com>
2024-11-17 17:43:19 -0300

    mudar botao se logado

--- src/components/actionButtons/actionButtons.tsx
+++ src/components/actionButtons/actionButtons.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import Link from "next/link";
 import { Button } from '../ui/button';
 import { Heart, LogIn, MessageSquare } from 'lucide-react';
+import { AuthContext } from '@/contexts/AuthContext';
 
 interface ActionButtonsProps {
   className?: string;
 }
 
 const ActionButtons: React.FC<ActionButtonsProps> = ({ className }) => {
+
+  const authContext = React.useContext(AuthContext);
+  const isLoggedIn = authContext?.token ? true : false;
+
   return (
     <div className={className}>
       <Link href={''}>
@@ -24,12 +29,21 @@ const ActionButtons: React.FC<ActionButtonsProps> = ({ className }) => {
         </Button>
       </Link>
 
-      <Link href={'/signIn'}>
-        <Button variant="secondary" size="sm">
-          <LogIn className="mr-2 h-4 w-4" />
-          Login
-        </Button>
-      </Link>
+      {isLoggedIn ? (
+        <Link href={'/dashboard'}>
+          <Button variant="secondary" size="sm">
+            <LogIn className="mr-2 h-4 w-4" />
+            Dashboard
+          </Button>
+        </Link>
+      ) : (
+        <Link href={'/signIn'}>
+          <Button variant="secondary" size="sm">
+            <LogIn className="mr-2 h-4 w-4" />
+            Login
+          </Button>
+        </Link>
+      )}
     </div>
   );
 };
