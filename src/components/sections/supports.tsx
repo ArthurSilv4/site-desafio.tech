043e20ffac79069c1c7cd35cbf1577166dd6994f
Arthur De Souza Silva <arthurdesouza.silv@gmail.com>
2024-11-28 18:57:56 -0300

    correcao de responsividade

--- src/components/sections/supports.tsx
+++ src/components/sections/supports.tsx
@@ -22,17 +22,17 @@ const person = [
 export default function Supports() {
   return (
     <section className="w-full text-white py-12 md:py-24 lg:py-32">
-      <Separator className="mt-8 pt-8 border-t border-primary-foreground/10 text-center" />
-
-      <div className="container flex overflow-x-auto whitespace-nowrap justify-center items-center m-auto">
-        {person.map((person, index) => (
-          <div key={index} className="py-2 m-4 text-gray-400 text-2xl font-semibold inline-block">
-            {person.name}
-          </div>
-        ))}
+      <div className="container overflow-x-auto whitespace-nowrap m-auto">
+        <Separator className="mt-8 pt-8 border-t border-primary-foreground/10 text-center" />
+        <div className="flex items-center justify-center flex-wrap">
+          {person.map((person, index) => (
+            <div key={index} className="py-2 m-4 text-gray-400 text-2xl font-semibold">
+              {person.name}
+            </div>
+          ))}
+        </div>
+        <Separator className="mt-8 pt-8 border-t border-primary-foreground/10 text-center" />
       </div>
-
-      <Separator className="mt-8 pt-8 border-t border-primary-foreground/10 text-center" />
     </section>
   )
 }
