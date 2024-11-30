b7142d84c9879b5af627576d7d50b6dcf5242dd3
Arthur De Souza Silva <arthurdesouza.silv@gmail.com>
2024-11-30 17:51:49 -0300

    criado a section product

--- src/components/layout/main.tsx
+++ src/components/layout/main.tsx
@@ -3,12 +3,14 @@ import { LearnMore } from '../sections/learnMore'
 import { FrequentlyAskedQuestions } from '../sections/frequentlyAskedQuestions'
 import Hero from '../sections/hero'
 import Supports from '../sections/supports'
+import Product from '../sections/product'
 
 export default function Main() {
   return (
     <main className="flex flex-col min-h-screen">
       <Hero />
       <Supports />
+      <Product />
       <CreateYourChallenge />
       <LearnMore />
       <FrequentlyAskedQuestions />
