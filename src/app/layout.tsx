b1bd21058f186f0a5c463e58a4c3dcbc07389ba5
Arthur De Souza Silva <arthurdesouza.silv@gmail.com>
2024-11-30 21:10:22 -0300

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Footer from "@/components/layout/footer";
 import Header from "@/components/layout/header";
 import { AuthProvider } from "@/contexts/AuthContext";
+import { Analytics } from "@vercel/analytics/react"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -34,6 +35,7 @@ export default function RootLayout({
         <AuthProvider>
           <Header />
           {children}
+          <Analytics />
           <Footer />
         </AuthProvider>
       </body>
