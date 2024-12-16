abc74c56b6dc3d9c54b8784fad4bc454447d6cb3
Arthur De Souza Silva <arthurdesouza.silv@gmail.com>
2024-12-16 02:15:58 -0300

    style: arrumado a responsividade do layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,34 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
-import Footer from "@/components/layout/footer";
-import Header from "@/components/layout/header";
-import { AuthProvider } from "@/contexts/AuthContext";
+import type { Metadata } from "next"
+import localFont from "next/font/local"
+import "./globals.css"
+import Footer from "@/components/layout/footer"
+import Header from "@/components/layout/header"
+import { AuthProvider } from "@/contexts/AuthContext"
 import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/next"
-import SideBarr from "@/components/sideBar/sideBar";
-import { SidebarProvider } from "@/components/ui/sidebar";
+import SideBarr from "@/components/sideBar/sideBar"
+import { SidebarProvider } from "@/components/ui/sidebar"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
-});
+})
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
-});
+})
 
 export const metadata: Metadata = {
   title: "Desafio.Tech",
   description: "Desafios de programação para desenvolvedores",
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="pt-br">
@@ -37,22 +37,18 @@ export default function RootLayout({
       >
         <AuthProvider>
           <SidebarProvider>
-
             <SideBarr />
-            <div className="m-auto w-full h-full">
-              
+            <main className="m-auto h-full w-full overflow-hidden">
               <Header />
 
               {children}
               <Analytics />
               <SpeedInsights />
               <Footer />
-            </div>
-
+            </main>
           </SidebarProvider>
-
         </AuthProvider>
       </body>
     </html>
-  );
+  )
 }
